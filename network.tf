0be17f2a4de406a45629392d712715d1a1b4cb2a
joshuaperakis <jvperakis@comcast.net>
2023-04-06 13:09:45 -0500

    add missing param

--- network.tf
+++ network.tf
@@ -2,5 +2,6 @@ module "network" {
   source  = "app.terraform.io/tform-wkshp/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
+  resource_group_name   = azurerm_resource_group.myresourcegroup.name
+  use_for_each          = false
 }
